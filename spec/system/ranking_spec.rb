2cbaa9471893d72102c6249eb400b1b457b0561c
Junichi Kobayashi <nnujabok@gmail.com>
2018-12-06 16:57:21 +0900

    wip
    Tasks carried over are:
    
    - create TargetChart
    - pass tests

--- spec/system/ranking_spec.rb
+++ spec/system/ranking_spec.rb
@@ -10,8 +10,12 @@ RSpec.describe 'Ranking', type: :system do
     fill_in '開始日時', with: Time.current
     fill_in '終了日時', with: Time.current.ago(1.month)
 
+    select 'beatmaniaIIDX', from: 'ranking_game'
+    select '冥', from: 'ranking_music'
+    select 'ANOTHER', from: 'ranking_chart'
+
     click_on '作成'
 
-    expect(page).to have_current_path(ranking_path(Ranking.all.reload.last))
+    expect(page).to have_content '作成されました'
   end
 end
