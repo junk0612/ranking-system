59b70b8f6a38d44627ee7d00923d7ec9b3b1b1ab
Junichi Kobayashi <nnujabok@gmail.com>
2018-12-27 11:09:18 +0900

    Update spec

--- spec/system/ranking_spec.rb
+++ spec/system/ranking_spec.rb
@@ -10,9 +10,9 @@ RSpec.describe 'Ranking', type: :system do
     fill_in '開始日時', with: Time.current
     fill_in '終了日時', with: Time.current.ago(1.month)
 
-    select 'beatmaniaIIDX', from: 'ranking_game'
-    select '冥', from: 'ranking_music'
-    select 'ANOTHER', from: 'ranking_chart'
+    select 'beatmaniaIIDX', from: 'Game'
+    select '冥', from: 'Music'
+    select 'ANOTHER', from: 'Chart'
 
     click_on '作成'
 
